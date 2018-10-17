35ca6acdd5d7ba8c3fb2689656df3d86ad85d157
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-10-17 11:58:42 -0700

    Making the pages (components) top (root) level components i.e. their own containers

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,48 +1,12 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from './FullPost/FullPost';
-import NewPost from './FullPost/NewPost/NewPost';
 import classes from './Blog.module.css';
 
+import Posts from './Posts/Posts';
+
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    };
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => (
-                    {...post, author: 'Kalesh'}
-                ));
-                this.setState({posts: updatedPosts});
-            })
-            .catch(error => {
-                console.log(error.message);
-                this.setState({error: true});
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    };
 
     render() {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post =>
-                (<Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />));
-        }
 
         return (
             <div className={classes.Blog}>
@@ -54,15 +18,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className={classes.Posts}>
-                    {posts}
-                </section>
-                {/*<section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost/>
-                </section>*/}
+                <Posts/>
             </div>
         );
     }
