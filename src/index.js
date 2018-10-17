3bf1a95844bc7fd3c689ad131939852384fce2d6
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-10-17 08:24:30 -0700

    Project setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(requestConfig => {
+    console.log(requestConfig);
+    // We can edi the request config here.
+    return requestConfig;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(responseConfig => {
+    console.log(responseConfig);
+    // We can edi the request config here.
+    return responseConfig;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content'] = 'application/json';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
